f27cef5d0b06c135a4b445c1422c75e94df0cbdf
JuhanaKivela <kivelajuhana@gmail.com>
2018-10-22 10:19:07 +0200

    my lifes work is there now

---
+++ Assets/OffGame/PlayerMovementTestScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTestScript : MonoBehaviour {
+
+    private float playerForwardSpeed = 6.0f;
+    private float playerSidewaysSpeed = 4.0f;
+    private float playerDownSpeed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.position = gameObject.transform.position + new Vector3(Input.GetAxis("Horizontal") * playerSidewaysSpeed * Time.deltaTime, playerDownSpeed*Time.deltaTime, playerForwardSpeed * Time.deltaTime);
+        if(Physics.Raycast(gameObject.transform.position,-gameObject.transform.up,1))
+        {
+            playerDownSpeed = 0;
+        }
+        else
+        {
+            playerDownSpeed = playerDownSpeed-0.5f;
+        }
+        if(Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+	}
+    void Jump()
+    {
+        gameObject.transform.position = gameObject.transform.position + new Vector3(1, 4, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerAutorunMovementScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAutorunMovementScript : MonoBehaviour
+{
+    //CharacterController can move the character easily with SimpleMove() or Move() method. We are using Move()
+    private CharacterController controller;
+    //The direction player will be moving. Vector3 = (x, y, z). Each value can be changed, without touching other values
+    private Vector3 moveDirection;
+
+    //Used for gravity setting
+    private bool isFalling = false;
+
+    float targetXLocation;
+    float currentXLocation;
+
+    //Called when the game starts
+    void Start ()
+    {
+        //Gets our players CharacterController and stores it to a variable. By this way we don't have to get the controller every frame below
+        controller = gameObject.GetComponent<CharacterController>();
+	}
+	
+	//Update is called every frame
+	void Update ()
+    {
+        //Checks if the player is on ground
+        if (controller.isGrounded)
+        {
+            isFalling = false;
+            //If player jumps, we set the direction y value to 8. This value determines, how high the player will jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = 10;
+            }
+        }
+        else 
+        {
+            //if character is not on ground and has no upward power left from jumping, it's is falling
+            if (isFalling==false&&moveDirection.y<=0)
+            {
+                isFalling = true;
+                moveDirection.y = 0;
+            }
+            //direction y is the gravity of the player. Changing the value below determines, how much gravity the player has
+            moveDirection.y = moveDirection.y - 20 * Time.deltaTime;
+        }
+
+        //Changes the direction x value (left/right). The value 2 determines, how fast the player can move sideways
+        moveDirection.x = Input.GetAxis("Horizontal") * 200 * Time.deltaTime;
+
+        //direction z determines, how fast the player will move forward
+        moveDirection.z = gameObject.transform.forward.z * 6;
+
+        //Finally the controller.Move() will update our players location
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
