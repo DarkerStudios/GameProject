8702edf8a546290a7417d3a7e6f6eb647d517da4
JuhanaKivela <kivelajuhana@gmail.com>
2018-11-15 11:03:40 +0200

    Finished Trap1

--- Assets/Scripts/FallTrapScript.cs
+++ Assets/Scripts/FallTrapScript.cs
@@ -7,13 +7,27 @@ public class FallTrapScript : MonoBehaviour {
     public GameObject holder1;
     public GameObject holder2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private float currentRotation = 0;
+    private readonly float rotationMultiplier = 400;
+    private readonly float maxRotation = 90;
+    private bool hasActivatedTrap = false;
+
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (hasActivatedTrap == true && currentRotation < maxRotation)
+        {
+            currentRotation = currentRotation + rotationMultiplier * Time.deltaTime;
+            holder1.transform.localRotation = Quaternion.Euler(currentRotation, 0, 0);
+            holder2.transform.localRotation = Quaternion.Euler(-currentRotation, 0, 0);
+        }
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            hasActivatedTrap = true;
+        }
+    }
 }
