d05fbb23dcf7ac63308ed4f622265fdff2d56bec
JuhanaKivela <32447947+JuhanaKivela@users.noreply.github.com>
2018-11-15 09:14:12 +0200

    Started making trap1

---
+++ Assets/Scripts/FallTrapScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTrapScript : MonoBehaviour {
+
+    public GameObject holder1;
+    public GameObject holder2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
