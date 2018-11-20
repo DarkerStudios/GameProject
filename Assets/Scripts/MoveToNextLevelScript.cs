bfb5fd7b24b51bebf07ff6ac03bf6ec6ca82514e
TumbeT <42261921+TumbeT@users.noreply.github.com>
2018-11-20 10:26:44 +0200

    Moving to next level

---
+++ Assets/Scripts/MoveToNextLevelScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevelScript : MonoBehaviour {
+
+    private int nextScene;
+	// Use this for initialization
+	void Start () {
+        nextScene = SceneManager.GetActiveScene().buildIndex +1;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+        
+    }
+   
+}
