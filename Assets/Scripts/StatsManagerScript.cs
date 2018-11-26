02dec17a8f8218d9a232595338e95795c50f7939
JuhanaKivela <kivelajuhana@gmail.com>
2018-11-26 13:00:58 +0200

    Updated Levels + started StatsManager

---
+++ Assets/Scripts/StatsManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManagerScript : MonoBehaviour {
+
+    private int coinAmount = 0;
+
+	// Use this for initialization
+	void Start () {
+		coinAmount = PlayerPrefs.GetInt("CoinCount")
+	}
+	
+}
