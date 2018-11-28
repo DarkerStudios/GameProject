4ab1854a6950be980af3fc99de74e46b644d5935
JuhanaKivela <kivelajuhana@gmail.com>
2018-11-28 15:40:39 +0200

    Added pause button + edited canvas

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    private float gameSpeed = 0.8f;
+
+    //Pause game
+    public GameObject pausedGameGameObject;
+    public GameObject removedWhenPausedGameObject;
+
+	void Start ()
+    {
+        Time.timeScale = gameSpeed;
+        Application.targetFrameRate = 60;
+    }
+
+    public void ContinueGame()
+    {
+        pausedGameGameObject.SetActive(false);
+        removedWhenPausedGameObject.SetActive(true);
+        Time.timeScale = gameSpeed;
+    }
+
+    public void PauseGame()
+    {
+        removedWhenPausedGameObject.SetActive(false);
+        pausedGameGameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
