e99b328fcc651f8efb7faecbea072e6e6dafe01e
TumbeT <42261921+TumbeT@users.noreply.github.com>
2018-12-04 13:36:09 +0200

    Modified level 1-3 and added rockwall

--- Assets/Scripts/PlayerDeathScript.cs
+++ Assets/Scripts/PlayerDeathScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeathScript : MonoBehaviour {
-
+    public float deathHeight;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +11,7 @@ public class PlayerDeathScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(gameObject.transform.position.y < 10)
+		if(gameObject.transform.position.y < deathHeight)
         {
             Instantiate(gameObject, new Vector3(0, 20, 0), Quaternion.identity);
             Destroy(gameObject);
