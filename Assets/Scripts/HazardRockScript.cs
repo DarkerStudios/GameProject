1d73cc9208ebf83f47ee1bd03286f9ad340c6994
JuhanaKivela <kivelajuhana@gmail.com>
2018-12-11 09:34:12 +0200

    Desrt hazard rocks

---
+++ Assets/Scripts/HazardRockScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardRockScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, 20, 0);
+            Destroy(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAutorunMovementScript>());
+            GameObject.FindGameObjectWithTag("Player").AddComponent<PlayerAutorunMovementScript>();
+        }
+    }
+}
